4926183a05acb23a66cf9b8c6f6f9006d5c3d536
Karl Steven F. Requinto <lerdrobott@gmail.com>
2023-06-10 01:02:25 +0800

    post-detail page

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.starting_page, name="starting-page"),  # starting page
     path("posts", views.posts, name="posts-page"), # dynamic segment (/posts/my-first-post)
-    path("posts/<slug>", views.post_detail, name="post-detail-page")
+    path("posts/<slug:slug>", views.post_detail, name="post-detail-page")
 ]
