8c0e1c132b188804261a719017185a1df43f24be
Karl Steven F. Requinto <lerdrobott@gmail.com>
2023-06-16 18:23:27 +0800

    change python version and virtual envi to install Pillow

--- my_django_blog/settings.py
+++ my_django_blog/settings.py
@@ -124,6 +124,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+MEDIA_ROOT = BASE_DIR / "uploads"
+MEDIA_URL = "/fields/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
