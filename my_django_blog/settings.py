3e746b8d1375e601fef89bffb00da1cd79dcc4ff
Karl Steven F. Requinto <lerdrobott@gmail.com>
2023-06-19 21:42:56 +0800

    adding requirements.txt, setting up environment variables

--- my_django_blog/settings.py
+++ my_django_blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t!&cd2g%q01)j-$@cegbj4d&q=vmbo4z+nz41u#@)99k+^d)^)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv("IS_DEVELOPMENT",True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    getenv("APP_HOST")
+]
 
 
 # Application definition
